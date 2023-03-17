0cc48804593626ed477984ca8af4d750d4fee9aa
mshaheerz <mshaheerkp9@gmail.com>
2023-03-17 16:11:45 +0530

    cors error

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -6,8 +6,11 @@ const connectDb = async (DATABASE_URL) => {
     mongoose.set("strictQuery", false);
         try{
             const DB_OPTIONS ={dbName:'seeker'}
-            await mongoose.connect(DATABASE_URL,DB_OPTIONS )
-            console.log('connected successfully..');
+            await mongoose.connect(DATABASE_URL,DB_OPTIONS,(err)=>{
+                if(err) console.log(err)
+                else console.log("connecteddd")
+            } )
+            
         }catch(error){
     
             console.log(error);
